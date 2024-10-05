54c7ce5dfeb084e72fa61f94a761ea43b7384b6d
root <root@AnthonyASUS>
2024-10-05 01:16:40 -0400

    customized paramertized test

--- tests/test_calculation_class.py
+++ tests/test_calculation_class.py
@@ -4,14 +4,14 @@ from calculator.calculation_class import calculation_class
 from calculator.arithmetic_operations import add, subtract, multiply, divide
 
 @pytest.mark.parametrize("a, b, operation, expected", [
-    (Decimal('10'), Decimal('5'), add, Decimal('15')),  # Test addition
-    (Decimal('10'), Decimal('5'), subtract, Decimal('5')),  # Test subtraction
-    (Decimal('10'), Decimal('5'), multiply, Decimal('50')),  # Test multiplication
-    (Decimal('10'), Decimal('2'), divide, Decimal('5')),  # Test division
-    (Decimal('10.5'), Decimal('0.5'), add, Decimal('11.0')),  # Test addition with decimals
-    (Decimal('10.5'), Decimal('0.5'), subtract, Decimal('10.0')),  # Test subtraction with decimals
-    (Decimal('10.5'), Decimal('2'), multiply, Decimal('21.0')),  # Test multiplication with decimals
-    (Decimal('10'), Decimal('0.5'), divide, Decimal('20')),  # Test division with decimals
+    (Decimal('20'), Decimal('10'), add, Decimal('30')),  # Test addition
+    (Decimal('20'), Decimal('10'), subtract, Decimal('10')),  # Test subtraction
+    (Decimal('20'), Decimal('10'), multiply, Decimal('200')),  # Test multiplication
+    (Decimal('20'), Decimal('2'), divide, Decimal('10')),  # Test division
+    (Decimal('20.5'), Decimal('5.5'), add, Decimal('26.0')),  # Test addition with decimals
+    (Decimal('20.5'), Decimal('5.5'), subtract, Decimal('15.0')),  # Test subtraction with decimals
+    (Decimal('20.5'), Decimal('5'), multiply, Decimal('102.5')),  # Test multiplication with decimals
+    (Decimal('21.0'), Decimal('10.5'), divide, Decimal('2.0')),  # Test division with decimals
 ])
 
 def test_calculation_operations(a, b, operation, expected):
